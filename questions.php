a3a6c26d90472a4affb98c53bf5ff4e02b26c787
16711731 <53744819+16711731@users.noreply.github.com>
2019-08-05 09:05:16 +0100

    Update questions

--- questions.php
+++ questions.php
@@ -26,6 +26,18 @@
     </form>
 */
 
+if (!empty($_POST)) {
+    $email = $_POST["email"];
+    $name = $_POST["name"];
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
+        $statement = $dbh->prepare("INSERT INTO Users ('name', 'email') VALUES (:name, :email)");
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+    }
+}
+
 /*
 QUESTION 2
  * Based on the data table below, please provide MySQL examples for the following requests:
@@ -46,10 +58,32 @@ QUESTION 2
     +--------+-----------+--------+--------------+
 */
 
+$statement = $dbh->query("SELECT * FROM Users");
+$statement = $dbh->query("UPDATE Users SET 'name' = 'Will' WHERE id = 4");
+$statement = $dbh->query("DELETE FROM Users WHERE id = 4");
+
 /*
 QUESTION 3
  * Looking at the above data table, suggest some appropriate data types and indexes for the columns.
 */
+
+
+id
+INT UNSIGNED NOT NULL AUTO_INCREMENT
+
+name
+VARCHAR(255) NULL DEFAULT NULL
+
+gender
+ENUM('Male', 'Female', 'Other') NOT NULL
+
+is_logged_in
+TINYINT(1) NOT NULL DEFAULT 0
+
+Indexes
+PRIMARY KEY (`id`)
+INDEX `gender` (`gender`)
+
 
 /*
 QUESTION 4
@@ -72,11 +106,29 @@ QUESTION 4
     +-------------+----------+--------------+
 */
 
+$statement = $dbh->query("
+    SELECT u.name, usp.sport
+    FROM Users u
+    LEFT JOIN UserSports usp ON u.id = usp.user_id
+    WHERE usp.is_favourite = 1
+");
+
 /*
 QUESTION 5
  * Describe in your own words what you would need to do if you wanted to list the name of
    each person alongside all the sports that they play separated by a comma (,)?
 */
+
+
+- Use the query result above
+- You should get back a list of users and sports with the user being duplicated
+- Initialise a new array to store user sports (starts empty)
+- Start a foreach loop of the $users array as $user
+- Every loop cycle, put the user name as an array index into the empty array
+- The value of the array is an array of sports (push one to the list for every user sport)
+- When the foreach is done, another loop is needed to concatenate the array of sports
+- Implode the sports arrays by comma, converting each array to a comma separated string
+
 
 /*
 QUESTION 6
@@ -121,6 +173,61 @@ $recipes = [
     ]
 ];
 
+
+/** 
+ * Recipe ingredients list
+ * Cost: in pence (simpler to store currency as integer)
+ * Amount: integer
+ * Measurement unit: split out from the amount, to stop data pollution
+ */
+
+$recipes = [
+    "Spindogs Magic Drink" => [
+        "Sugar" => [
+            "Cost" => 50,                
+            "Amount" => 100,
+            "Unit" => "g"
+        ],
+        "Chocolate" => [
+            "Cost" => 150,
+            "Amount" => 100,
+            "Unit" => "g"
+        ],
+        "Squash" => [
+            "Cost" => 80,
+            "Amount" => 100,
+            "Unit" => "ml"
+        ],
+        "Coffee" => [
+            "Cost" => 200,
+            "Amount" => 100,
+            "Unit" => "ml"
+        ]
+    ],
+    "Spindogs Punch" => [
+        "Rum" => [
+            "Cost" => 180,
+            "Amount" => 100,
+            "Unit" => "gram"
+        ],
+        "Vodka" => [
+            "Cost" => 150,
+            "Amount" => 100,
+            "Unit" => "ml"
+        ],
+        "Orange Juice" => [
+            "Cost" => 90,
+            "Amount" => 100,
+            "Unit" => "ml"
+        ],
+        "Lime" => [
+            "Cost" => 75,
+            "Amount" => 1,
+            "Unit" => "slice(s)"
+        ]
+    ]
+];
+
 /*
 QUESTION 8
  * Write some PHP code to loop through your array and show the following:
@@ -128,6 +235,24 @@ QUESTION 8
  * b) Display the cost of each recipe
  * c) Display the total cost of **all recipes**
 */
+
+$totalCost = 0;
+
+foreach ($recipes as $recipe => $ingredients) {
+    $recipeCost = 0;
+
+    echo $recipe . "\n";
+
+    foreach ($ingredients as $ingredient => $value) {
+        $totalCost += $ingredient['Cost'];
+        $recipeCost += $ingredient['Cost'];
+        echo "- " . $ingredient . " (" . $value['Amount'] . $value['Unit'] . ")\n";
+    }
+
+    echo "\nRecipe cost: £" . number_format(($recipeCost / 100), 2, '.', ' ') . "\n";
+}
+
+echo "\nTotal cost: £" . number_format(($totalCost / 100), 2, '.', ' ');
 
 /*
 QUESTION 9
@@ -166,6 +291,11 @@ QUESTION 11
 */
 
 /*
+- Objects can have properties, methods, and have their own type
+- Classes are like blueprints for objects, you can create an instance of an object using them
+*/
+
+/*
 QUESTION 12
  * Write a single PHP class for a "Bear" (with approx. 50 lines of code). This is your opportunity
    to demonstrate your OOP understanding and coding style. You get to determine what properties and
@@ -173,3 +303,53 @@ QUESTION 12
  * a) Eat honey every 2 hours and remember when they last had honey
  * b) Decide if they need to sleep
 */
+
+class Bear
+{
+    const HOURS_PER_EAT = 2;
+    const HOURS_PER_SLEEP = 12;
+    private $timeLastEaten;
+    private $timeLastSlept;
+    
+    __construct()
+    {
+        $this->timeLastEaten = time();
+        $this->timeLastSlept = time();
+    }
+    
+    private function isHungry()
+    {
+        $secondsPerEat = self::HOURS_PER_EAT * 1000;
+        if( time() > ($this->timeLastEaten + $secondsPerEat) ) {
+            return true;
+        }
+    }
+
+    private function isTired()
+    {
+        $secondsPerSleep = self::HOURS_PER_SLEEP * 1000;
+        if( time() > ($this->timeLastSlept + $secondsPerSleep) ) {
+            return true;
+        }
+    }
+
+    public function update()
+    {
+        if ($this->isHungry()) {
+            $this->timeLastEaten = time();
+            echo "I will now eat\n";
+        }
+
+        if ($this->isTired()) {
+            $this->timeLastSlept = time();
+            echo "I am tired\n";
+        }
+    }
+
+}
+
+$bear = new Bear();
+
+while (true) {
+    $bear->update();
+}
